05c736e33d44a0a9e26aa9a7d28d8bca31631e91
austin shieh <macuser@MacBook-Pro-3.local>
2012-10-28 22:11:10 -0500

    cucumber for search

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,27 +2,45 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    @movie = Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+
+  #puts movies_table.hashes
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert(/.*#{e1}.*#{e2}/m =~ page.body)
+  #puts page.body
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+Then /I should see all of the movies/ do
+   assert(page.all('#movies tr').size-1 == Movie.count)
+end
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list = rating_list.split(' ')
+  rating_list.each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings[#{rating}]"}
+    else
+      step %Q{I check "ratings[#{rating}]"}
+    end
+  end
+
 end
